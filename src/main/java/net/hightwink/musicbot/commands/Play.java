2ab4a3f9e02306242e38868c0415390ab4b89e21
Starrysparklez <50998351+Starrysparklez@users.noreply.github.com>
2021-12-24 23:42:56 +0800

    Create /play command

---
+++ src/main/java/net/hightwink/musicbot/commands/Play.java
@@ -0,0 +1,67 @@
+package net.hightwink.musicbot.commands;
+
+import com.sedmelluq.discord.lavaplayer.player.FunctionalResultHandler;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.hightwink.musicbot.classes.*;
+
+public class Play extends SlashCommandExecutor {
+    public Play() {
+        this.commandName = "play";
+        this.description = "Начать проигрывание музыки в голосовом канале.";
+        this.commandData = new CommandData(commandName, description)
+                .addOption(OptionType.STRING, "query", "Ваш запрос", true);
+    }
+
+    @Override
+    public void execute(Context ctx) {
+        GuildVoiceState userState = ctx.getAuthor().getVoiceState();
+        if (userState == null || userState.getChannel() == null) {
+            ctx.replyText("Вы должны находиться в голосовом канале!").queue();
+            return;
+        }
+
+        Guild server = ctx.getGuild();
+        String query = ctx.getEvent().getOption("query").getAsString();
+
+        MusicPlayerManager manager = AudioManager.get(ctx.getGuild());
+        ctx.getGuild().getAudioManager().setSendingHandler(new AudioPlayerSendHandler(manager.getPlayer()));
+
+        server.getAudioManager().openAudioConnection(userState.getChannel());
+
+        PlayerManager.getManager().loadItemOrdered(manager,
+            query.startsWith("https://") ? query : "ytsearch: " + query,
+            new FunctionalResultHandler(audioTrack -> {
+                ctx.replyText("Трек **" + audioTrack.getInfo().title + "** добавлен в очередь.")
+                        .queue();
+                manager.getScheduler().queue(audioTrack);
+            }, audioPlaylist -> {
+                if (audioPlaylist.isSearchResult()) {
+                    AudioTrack first = audioPlaylist.getTracks().get(0);
+                    manager.getScheduler().queue(first);
+                    ctx.replyText("Трек **" + first.getInfo().title + "** добавлен в очередь.")
+                            .queue();
+                } else {
+                    audioPlaylist.getTracks().forEach(t -> {
+                        if (manager.getScheduler().getQueue().size() + 1 > 200) {
+                            ctx.replyText("Размер очереди превышен. Пожалуйста, не более 200 треков.")
+                                    .queue();
+                            return;
+                        }
+                        manager.getScheduler().queue(t);
+                    });
+                ctx.replyText("Плейлист **" + audioPlaylist.getName() + "** с "
+                                + audioPlaylist.getTracks().size() + " треками добавлен в очередь.")
+                        .queue();
+                }
+            }, () -> ctx.replyText("По запросу " + query + " ничего не найдено.").queue(),
+            error -> {
+                error.printStackTrace();
+                ctx.replyText("Ошибка воспроизведения.").queue();
+            })
+        );
+    }
+}
