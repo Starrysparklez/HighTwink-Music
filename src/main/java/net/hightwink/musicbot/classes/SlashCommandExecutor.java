aed2e3d0ab75c706d8171f694b0162c144ea40e3
Starrysparklez <50998351+Starrysparklez@users.noreply.github.com>
2022-01-14 04:27:29 +0700

    Add requiredPermissions field

--- src/main/java/net/hightwink/musicbot/classes/SlashCommandExecutor.java
+++ src/main/java/net/hightwink/musicbot/classes/SlashCommandExecutor.java
@@ -1,10 +1,16 @@
 package net.hightwink.musicbot.classes;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.hightwink.musicbot.exceptions.NonImportantException;
+
+import java.util.List;
 
 public abstract class SlashCommandExecutor {
     public String commandName;
     public String description;
     public CommandData commandData;
-    public void execute(Context ctx) throws NonImportantException {};
+    public List<Permission> requiredPermissions;
+
+    public void execute(Context ctx) throws NonImportantException {}
 }
