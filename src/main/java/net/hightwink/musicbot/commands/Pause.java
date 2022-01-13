2f252d019b58ffd6d8b7caa108225ff125cddf6b
Starrysparklez <50998351+Starrysparklez@users.noreply.github.com>
2022-01-14 04:28:58 +0700

    Add /pause command

---
+++ src/main/java/net/hightwink/musicbot/commands/Pause.java
@@ -0,0 +1,36 @@
+package net.hightwink.musicbot.commands;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.hightwink.musicbot.classes.Context;
+import net.hightwink.musicbot.classes.SlashCommandExecutor;
+import net.hightwink.musicbot.classes.audio.AudioManager;
+import net.hightwink.musicbot.classes.audio.MusicPlayerManager;
+import net.hightwink.musicbot.classes.utils.VoteManager;
+
+public class Pause extends SlashCommandExecutor {
+    public Pause() {
+        this.commandName = "pause";
+        this.description = "Поставить воспроизведение на паузу.";
+        this.commandData = new CommandData(this.commandName, this.description);
+    }
+
+    @Override
+    public void execute(Context ctx) {
+        if (ctx.getAuthor().getPermissions().contains(Permission.ADMINISTRATOR)) {
+            pausePlayback(ctx);
+            return;
+        }
+        VoteManager voteManager = new VoteManager(ctx);
+        voteManager.call("Голосование за паузу", c -> pausePlayback(ctx),
+                c -> ctx.replyText("Запрос на паузу отменен.").queue());
+    }
+
+    private void pausePlayback(Context ctx) {
+        MusicPlayerManager manager = AudioManager.get(ctx.getGuild());
+        AudioPlayer player = manager.getPlayer();
+        player.setPaused(!player.isPaused());
+        ctx.replyText(":ok_hand:").queue();
+    }
+}
