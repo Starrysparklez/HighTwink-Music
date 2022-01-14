330268495928c3e43241b1f18fd3895c70bc086a
Starrysparklez <50998351+Starrysparklez@users.noreply.github.com>
2022-01-14 20:14:49 +0700

    Add /shuffle command

---
+++ src/main/java/net/hightwink/musicbot/commands/Shuffle.java
@@ -0,0 +1,35 @@
+package net.hightwink.musicbot.commands;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.hightwink.musicbot.classes.Context;
+import net.hightwink.musicbot.classes.SlashCommandExecutor;
+import net.hightwink.musicbot.classes.audio.AudioManager;
+import net.hightwink.musicbot.classes.audio.MusicPlayerManager;
+import net.hightwink.musicbot.classes.utils.VoteManager;
+import net.hightwink.musicbot.exceptions.NonImportantException;
+
+public class Shuffle extends SlashCommandExecutor {
+    public Shuffle() {
+        this.commandName = "shuffle";
+        this.description = "Перемешать треки в очереди.";
+        this.commandData = new CommandData(this.commandName, this.description);
+    }
+
+    @Override
+    public void execute(Context ctx) throws NonImportantException {
+        if (ctx.getAuthor().getPermissions().contains(Permission.ADMINISTRATOR)) {
+            shufflePlayback(ctx);
+            return;
+        }
+        VoteManager voteManager = new VoteManager(ctx);
+        voteManager.call("Голосование за паузу", c -> shufflePlayback(ctx),
+                c -> ctx.replyText("Запрос на паузу отменен.").queue());
+    }
+
+    private void shufflePlayback(Context ctx) {
+        MusicPlayerManager manager = AudioManager.get(ctx.getGuild());
+        manager.getScheduler().shuffleQueue();
+        ctx.replyText(":twisted_rightwards_arrows: :ok_hand:").queue();
+    }
+}
