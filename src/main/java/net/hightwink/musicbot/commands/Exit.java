13698204a82170519eff7f3802a54c6477e67c7f
Starrysparklez <50998351+Starrysparklez@users.noreply.github.com>
2022-01-16 05:44:29 +0700

    Rename Restart.java

--- src/main/java/net/hightwink/musicbot/commands/Exit.java
+++ src/main/java/net/hightwink/musicbot/commands/Exit.java
@@ -1,11 +1,13 @@
 package net.hightwink.musicbot.commands;
 
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.hightwink.musicbot.classes.Context;
 import net.hightwink.musicbot.classes.SlashCommandExecutor;
 
-public class Restart extends SlashCommandExecutor {
-    public Restart() {
+public class Exit extends SlashCommandExecutor {
+    public Exit() {
         this.commandName = "exit";
         this.description = "Завершить работу данного инстанса бота.";
         this.commandData = new CommandData(this.commandName, this.description);
@@ -14,7 +16,9 @@ public class Restart extends SlashCommandExecutor {
 
     @Override
     public void execute(Context ctx) {
-        ctx.replyText("Выключаемся...").queue();
-        System.exit(0);
+        if (ctx.getAuthor().getPermissions().contains(Permission.ADMINISTRATOR)) {
+            ctx.replyText("Выключаемся...");
+            System.exit(0);
+        }
     }
 }
